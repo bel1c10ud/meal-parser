512469fbeda2854476e313ee7fa693e6e44411a4
bel1c10ud <mail@bellclou.de>
2021-02-15 19:34:49 +0900

    move viewport meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import Head from 'next/head';
 import NProgress from 'nprogress';
 
 import '../styles/globals.css'
@@ -10,7 +11,17 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,6 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-          />
           <meta name="description" content="Description" />
           <meta name="keywords" content="Keywords" />
           <link rel="manifest" href="/manifest.json" />
